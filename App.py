76b1a573fdf2b0cfbe455afb4efc09804f305835
KD131 <71063280+KD131@users.noreply.github.com>
2023-05-21 03:07:03 +0200

    adds searched attractions to map

--- App.py
+++ App.py
@@ -7,35 +7,42 @@ import db
 
 st.set_page_config(layout="wide")
 
+NAME_PROPERTIES = ['name', 'scen_lm_na', 'area_name', 'lpc_name']
+
+def get_name(feature):
+    properties = feature['properties']
+    return properties.get('name') or properties.get('scen_lm_na') or properties.get('area_name') or properties.get('lpc_name')
+
+def remove_ids(features):
+    for feature in features:
+        feature.pop('_id', None)
+
+
 st.title("Welcome to the NYC Transit App")
 st.write("This app shows you NYC subway stops and nearby attractions")
 # get data
 subway_stops = db.subway_stops.get_all()
-for stop in subway_stops['features']:
-    del stop['_id']
-attractions = db.attractions.get_all()
-all = FeatureCollection(subway_stops['features'] + attractions['features'])
+remove_ids(subway_stops['features'])
 
 col1, col2 = st.columns(2)
-
 with col1:
     # subway dropdown
     subway = st.selectbox("Select a subway stop", subway_stops['features'], format_func=lambda stop: stop['properties']['name'])
-    nearby_attractions = db.attractions.get_near(subway['geometry']['coordinates'])
-    st.write(f"{len(nearby_attractions['features'])} attractions found near this stop.")
-    def get_name(feature):
-        properties = feature['properties']
-        return properties.get('name') or properties.get('scen_lm_na') or properties.get('area_name') or properties.get('lpc_name')
-
-    with st.expander("Show attractions"):
-        st.write([get_name(attraction) for attraction in nearby_attractions['features']])
+    # unfortunately it always selects something anyway.
+    if subway:
+        nearby_attractions = db.attractions.get_near(subway['geometry']['coordinates'])
+        st.write(f"{len(nearby_attractions['features'])} attractions found near this stop.")
+        with st.expander("Show attractions"):
+            st.write([get_name(attraction) for attraction in nearby_attractions['features']])
 
     # attaction search
     name = st.text_input("Search for an attraction")
-    searched = db.attractions.get_like(name)
-    st.write(f"{len(searched['features'])} results found.")
-    with st.expander("Show attractions"):
-        st.write([get_name(attraction) for attraction in searched['features']])
+    if name:
+        searched = db.attractions.get_like(name)
+        remove_ids(searched['features'])
+        st.write(f"{len(searched['features'])} results found.")
+        with st.expander("Show attractions"):
+            st.write([get_name(attraction) for attraction in searched['features']])
 
 with col2:
     # create map
@@ -49,5 +56,21 @@ with col2:
                    popup=folium.GeoJsonPopup(fields=["name", "line", "notes"], aliases=["Stop Name", "Line", "Notes"]),
                    marker=folium.Circle(radius=20, fill=True, fill_opacity=0.8)
                    ).add_to(m)
+    
+    style = {
+        'fill': True,
+        'color': 'red',
+        'fill_color': 'red',
+        'fill_opacity': 0.8
+    }
+    
+    if name and searched:
+        folium.GeoJson(searched,
+                    style_function=lambda x: style,
+                    name="Search Results",
+                    tooltip=folium.GeoJsonTooltip(fields=['name'], aliases=["Name"]),
+                    marker=folium.Circle(radius=30, **style)
+                    ).add_to(m)
+    # folium.LayerControl(collapsed=False).add_to(m) # doesn't seem to show up
     map_data = st_folium(m, width=1200, height=800, returned_objects=[])
 
